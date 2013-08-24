bfa070eb542f05262524e26462f2675a5ad00ce3
abbad <ahm.abbad@gmail.com>
2013-08-24 21:50:18 +0300

    installed app and added models

--- jamhub/models.py
+++ jamhub/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Role(models.Model):
+	name = models.CharField(max_length = 30) 
+	
+	def __unicode__(self):
+		return self.name
+
+class Instrument(models.Model):
+	name = models.CharField(max_length = 30, unique = True)
+	
+	class Meta:
+		ordering = ['name']
+		
+	def __unicode__(self):
+		return self.name
+
+class DAW(models.Model):
+	name = models.CharField(max_length = 30, unique = True)
+	
+	class Meta: 
+		ordering = ['name']
+	
+	def __unicode__(self):
+		return self.name
+		
+class Profile(models.Model):
+	user = models.ForeignKey(User)
+	role = models.ManyToManyField(Role)
+	date_created = models.DateTimeField(auto_now_add = True)
+	instruments = models.ManyToManyField(Instrument)
+	software = models.ManyToManyField(DAW)
+	
+	def __unicode__(self):
+		return self.user
+		
+class Project(models.Model):
+	artist = models.ManyToManyField(Profile)
+	name = models.CharField(max_length = 30)
+	num_stars = models.IntegerField()
+	date_created = models.DateTimeField(auto_now_add = True)
+	genre = models.CharField(max_length = 30)
+	
+	def __unicode__(self):
+		return self.name
