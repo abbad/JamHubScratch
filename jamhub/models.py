1442e6f589e21c162132ef8ec2977882ead3e82b
abbad <ahm.abbad@gmail.com>
2013-08-25 18:24:35 +0300

    added a model form for profile

--- jamhub/models.py
+++ jamhub/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 class Role(models.Model):
 	name = models.CharField(max_length = 30) 
@@ -34,6 +35,11 @@ class Profile(models.Model):
 	
 	def __unicode__(self):
 		return self.user
+
+class ProfileForm(ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['user', 'role', 'instruments', 'software']
 		
 class Project(models.Model):
 	artist = models.ManyToManyField(Profile)
