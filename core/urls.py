9d3b72cadfc02c416105816264a7174fc14e7f45
abbad <ahm.abbad@gmail.com>
2013-08-25 04:23:43 +0300

    implemented first view with a template

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'xx.views.home', name='home'),
+    url(r'^$', 'jamhub.views.home', name='home'),
     # url(r'^xx/', include('xx.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
