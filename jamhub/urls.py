0979ac33e770b36c66ab4655a3da087d7073104f
abbad <ahm.abbad@gmail.com>
2013-08-26 20:55:57 +0300

    added a new url for show profile

--- jamhub/urls.py
+++ jamhub/urls.py
@@ -4,5 +4,7 @@ urlpatterns = patterns('jamhub.views',
 	(r'^createProfile', 'create_profile'),
 	(r'^logout', 'logout_view'),
 	(r'^home', 'home'),
+	(r'^showProfile', 'show_profile'),
 	(r'^test', 'test'),
+	
 )
